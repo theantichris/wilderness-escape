0c4791fcb214237f552424b1421af08b9cac115e
Christopher Lamm <chris@theantichris.com>
2019-12-13 09:19:48 -0500

    added more story choices

--- script.py
+++ script.py
@@ -21,8 +21,6 @@ class TreeNode:
             print(chosen_child)
             story_node = chosen_child
 
-user_choice = input("What is your name? ")
-
 story_root = TreeNode("""
 You are in a forest clearing. There is a path to the left.
 A bear emerges from the trees and roars!
@@ -40,6 +38,20 @@ Do you:
 2 ) Yell 'Hooray!'
 """)
 
+choice_a_1 = TreeNode("""
+The bear returns and tells you it's been a rough week. After making peace with
+a talking bear, he shows you the way out of the forest.
+
+YOU HAVE ESCAPED THE WILDERNESS.
+""")
+
+choice_a_2 = TreeNode("""
+The bear returns and tells you that bullying is not okay before leaving you alone
+in the wilderness.
+
+YOU REMAIN LOST.
+""")
+
 choice_b = TreeNode("""
 You come across a clearing full of flowers. 
 The bear follows you and asks 'what gives?'
@@ -49,8 +61,25 @@ Do you:
 2 ) Explain that the bear scared you.
 """)
 
+choice_b_1 = TreeNode("""
+The bear is unamused. After smelling the flowers, it turns around and leaves you alone.
+
+YOU REMAIN LOST.
+""")
+
+choice_b_2 = TreeNode("""
+The bear understands and apologizes for startling you. Your new friend shows you a 
+path leading out of the forest.
+
+YOU HAVE ESCAPED THE WILDERNESS.
+""")
+
 story_root.add_child(choice_a)
 story_root.add_child(choice_b)
+choice_a.add_child(choice_a_1)
+choice_a.add_child(choice_a_2)
+choice_b.add_child(choice_b_1)
+choice_b.add_child(choice_b_2)
 
 print("\nOnce upon a time...")
 story_root.traverse()
