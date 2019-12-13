b935537608cab6ab45508125484d2e1c9c05a905
Christopher Lamm <chris@theantichris.com>
2019-12-13 08:56:17 -0500

    added story root

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
-######
-# TREENODE CLASS
-######
 class TreeNode:
     def __init__(self, story_piece):
         self.story_piece = story_piece
         self.choices = []
+
+    def __repr__(self):
+        return self.story_piece
 
 ######
 # VARIABLES FOR TREE
@@ -14,3 +14,14 @@ class TreeNode:
 # TESTING AREA
 ######
 print("Once upon a time...")
+
+story_root = TreeNode(
+"""
+You are in a forest clearing. There is a path to the left.
+A bear emerges from the trees and roars!
+Do you: 
+1 ) Roar back!
+2 ) Run to the left...
+"""
+)
+print(story_root)
