fd01b61820aa76a8ec46861ec9f04fa4921551ea
Christopher Lamm <chris@theantichris.com>
2019-12-13 08:57:39 -0500

    cleanup

--- script.py
+++ script.py
@@ -7,15 +7,13 @@ class TreeNode:
         return self.story_piece
 
 
-story_root = TreeNode(
-"""
+story_root = TreeNode("""
 You are in a forest clearing. There is a path to the left.
 A bear emerges from the trees and roars!
 Do you: 
 1 ) Roar back!
 2 ) Run to the left...
-"""
-)
+""")
 
 print("Once upon a time...")
 print(story_root)
