77c557560d14cd06d138541c5c1bdf26b1ecad4e
Christopher Lamm <chris@theantichris.com>
2019-12-13 09:04:17 -0500

    added story choices

--- script.py
+++ script.py
@@ -6,6 +6,9 @@ class TreeNode:
     def __repr__(self):
         return self.story_piece
 
+    def add_child(self, node):
+        self.choices.append(node)
+
 user_choice = input("What is your name? ")
 
 story_root = TreeNode("""
@@ -15,6 +18,24 @@ Do you:
 1 ) Roar back!
 2 ) Run to the left...
 """)
+
+choice_a = TreeNode("""
+The bear is startled and runs away.
+Do you:
+1 ) Shout 'Sorry bear!'
+2 ) Yell 'Hooray!'
+""")
+
+choice_b = TreeNode("""
+You come across a clearing full of flowers. 
+The bear follows you and asks 'what gives?'
+Do you:
+1 ) Gasp 'A talking bear!'
+2 ) Explain that the bear scared you.
+""")
+
+story_root.add_child(choice_a)
+story_root.add_child(choice_b)
 
 print("Once upon a time...")
 print(story_root)
