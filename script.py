d526409da342e96cac1505b08dff6c105f41b1ff
Christopher Lamm <chris@theantichris.com>
2019-12-13 08:51:05 -0500

    initializing TreeNode class

---
+++ script.py
@@ -0,0 +1,16 @@
+######
+# TREENODE CLASS
+######
+class TreeNode:
+    def __init__(self, story_piece):
+        self.story_piece = story_piece
+        self.choices = []
+
+######
+# VARIABLES FOR TREE
+######
+
+######
+# TESTING AREA
+######
+print("Once upon a time...")
